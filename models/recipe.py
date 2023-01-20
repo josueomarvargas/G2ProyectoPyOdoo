36fe18455faffa9f1ae33f4b2a17d9e89fb5ed27
jon <jon.novoa.alaiz@gmail.com>
2023-01-20 13:12:58 +0100

    funcionando hasta meter receta en el user

--- models/recipe.py
+++ models/recipe.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields
-
 class recipe(models.Model):
     _name ='g2modulojjg.recipe'
     name = fields.Char()
     idRecipe = fields.Char()
     preparation = fields.Text()
-    recipeType = fields.Selection([('OMNIVORO'),('VEGANA'),('VEGETARIANO')],string="Tipo Receta")
+    #recipeType = fields.Selection([('OMNIVORO'),('VEGANA'),('VEGETARIANO')],string="Tipo Receta")
     createDate = fields.Date()
     dietist1 = fields.Many2one('res.users',ondelete = 'cascade',string= "Dietist", required=True)
     foods = fields.Many2many('g2modulojjg.food', string = "Food")
     diets = fields.Many2many ('g2modulojjg.diet', string = "Diet")
     
-  
--- models/user.py
+++ models/user.py
@@ -21,8 +21,8 @@ class user(models.Model):
     isAdministrator = fields.Boolean()
     clients= fields.One2many('res.users','dni',string="Client")
     dietist= fields.Many2one('res.users',ondelete='set null',string="Dietist", index=True)
-    dietist_food= fields.One2many('g2modulojjg.food', 'idFood', string= "Food")
-    dietist_diest= fields.One2many('g2modulojjg.diet', 'idDiet', string= "Diest")
-    dietist_recipe= fields.One2many('g2modulojjg.recipe', 'idRecipe', string="Recipe")
-    client_diets=fields.Many2many('g2modulojjg.diet', 'idDiet', string= "Diet")
+    dietist_food= fields.One2many('g2modulojjg.food', 'dietist', string= "Food")
+    dietist_diest= fields.One2many('g2modulojjg.diet', 'dietits', string= "Diest")
+    dietist_recipe= fields.One2many('g2modulojjg.recipe', 'dietist1', string="Recipe")
+    client_diets=fields.Many2many('g2modulojjg.diet', string= "Diet")
  
