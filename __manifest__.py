f9cf9871cce3c502d279305097d2622a2b5f893f
josue <josue@Orlando>
2023-01-31 13:44:18 +0100

    modelo y vista

--- __manifest__.py
+++ __manifest__.py
@@ -29,7 +29,7 @@
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
-        #'views/foods.xml',
+        'views/foods.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
